acc8488498ff835c41ae6a3d89448be9b4cb435e
limaneto <neto_pereira1@hotmail.com>
2016-05-17 18:36:00 -0300

    bug fix

--- ufcavalan/settings.py
+++ ufcavalan/settings.py
@@ -109,5 +109,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
