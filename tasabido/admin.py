7c9e9b63e98531fb003a462491786731552d320a
limaneto <neto_pereira1@hotmail.com>
2016-05-20 23:34:36 -0300

    Model Subtopico consertado

--- tasabido/admin.py
+++ tasabido/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Duvida, Ajuda, Materia, Subtopico
+from .models import Duvida, Ajuda, Materia, Subtopico, User
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Duvida)
 admin.site.register(Ajuda)
 admin.site.register(Materia)
